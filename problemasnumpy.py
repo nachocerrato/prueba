d08c60e426526d3ec689c23bd073605580eac76c
nachocerrato <72860834+nachocerrato@users.noreply.github.com>
2021-07-21 18:47:45 +0200

    Add files via upload

---
+++ problemasnumpy.py
@@ -0,0 +1,28 @@
+# Dados 2 arrays numéricos de dimensiones similares, ¿cómo obtenerun array numérico en el que cada elemento es una suma de elementos de las 2 matrices numéricas?
+
+# Multiplica un array de tu elección por un vector escalar (1x1). Se puede multiplicar arrays con diferentes tamaños?
+
+# Crea una matriz de identidad (identity matrix), puedes buscar en google/documentación si no desconoces cómo hacerlo.
+
+# Trata de juntar verticalmente 2 arrays distintos de tu elección, cada uno conteniendo más de una columna, con vstack() (investiga)
+
+# Genera una secuencia de números pares aleatorios enteros, en un rango de 0 a 200
+
+# Utiliza la función append, con numpy, para poder añadir manualmente un array dentro de otro
+
+# Partiendo de ([1,1,1,15,6,7,8,3,3,5,5]), se te ocurre alguna forma de eliminar los duplicados dentro de este array?
+
+# Utiliza reshape para reordenar de 3 formas distintas un array de tu elección
+
+# Genera matrices aleatorias partiendo de funciones random que conozcas de numpy, en este caso queremos arrays no dataframes.
+
+# Haz que un array (3x3) que generes, intercambie la última fila por la primera y la primera por la segunda.
+
+# Dibuja un gráfico de los precios de cierre de un activo con matplotlib que tenga leyenda y título en los ejes, a continuación dibuja 3 medias con diferentes periodos, una de ellas exponencial.
+
+# Partiendo de lo realizado en la clase anterior, intenta hacer por tu cuenta el gráfico de P&L de la compra de Put, con una prima, activo y strike de tu elección.
+
+# Grafica la volatilidad de las FAANG para un periodo de 100 (días en este caso)
+
+# Utiliza el módulo histplot() de seaborn para graficar los resultados de una empresa de tu elección, utiliza el argumento opcional kde=True
+
